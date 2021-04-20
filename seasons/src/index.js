111d2ad715d59a0e014f7d43042310fb1facab61
Stefan <stefanmitov8@gmail.com>
2021-04-20 22:56:27 +0200

    finish section 5

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,17 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
 
+        this.state = { lat: null , errorMessage: '' }; 
+
+        window.navigator.geolocation.getCurrentPosition(
+            position =>  {
+                this.setState({ lat: position.coords.latitude });
+            },
+            error => {
+                this.setState({ errorMessage: error.message });
+            }
+        );
+    }
 
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position =>  console.log(position),
-            error => console.log(error)
-        )
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
 
-        return (
-            <div>Location: </div>
-        );
+        if (!this.state.errorMessage && this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>
+        }
+
+        return <div>Loading...</div>
     }
 }
 
