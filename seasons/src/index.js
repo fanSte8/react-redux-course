9ab839fa95b4b45e2babeed13a4129a7510ff9a5
Stefan <stefanmitov8@gmail.com>
2021-04-21 11:19:13 +0200

    finish section 6

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,32 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = { lat: null , errorMessage: '' };
 
-        this.state = { lat: null , errorMessage: '' }; 
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position =>  {
-                this.setState({ lat: position.coords.latitude });
-            },
-            error => {
-                this.setState({ errorMessage: error.message });
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            error => this.setState({ errorMessage: error.message })
         );
     }
 
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>
+            return <div>Error: {this.state.errorMessage}</div>;
         }
 
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>
+            return <SeasonDisplay lat={this.state.lat} />;
         }
 
-        return <div>Loading...</div>
+        return <Spinner message="Please allow location access" />;
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
