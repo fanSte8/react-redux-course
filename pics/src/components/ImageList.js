97a1119785c7f8d0a3ba8bc02725f126c0f61971
Stefan <stefanmitov8@gmail.com>
2021-04-21 17:07:00 +0200

    complete section 9

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const ImageList = (props) => {
-  return (
-    <div>
-      
-    </div>
-  );
+  const images = props.images.map(({id, description, urls}) => {
+    return <img key={id} alt={description} src={urls.regular} />
+  });
+
+  return <div>{images}</div>;
 }
 
 export default ImageList;
